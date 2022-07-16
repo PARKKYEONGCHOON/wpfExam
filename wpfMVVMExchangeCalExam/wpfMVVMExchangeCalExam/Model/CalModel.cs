5e7503def79a1e9d0a1c529c1084e8d671cdd853
PARKKYEONGCHOON <98440576+PARKKYEONGCHOON@users.noreply.github.com>
2022-07-16 22:35:33 +0900

    Add files via upload

---
+++ wpfMVVMExchangeCalExam/wpfMVVMExchangeCalExam/Model/CalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfMVVMExchangeCalExam.Model
+{
+    public class CalModel
+    {
+        public string dollar;
+        public string won;
+
+    }
+}
---
+++ wpfMVVMExchangeCalExam/wpfMVVMExchangeCalExam/ViewModel/CalViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using wpfMVVMExchangeCalExam.Model;
+using System.Windows;
+
+namespace wpfMVVMExchangeCalExam.ViewModel
+{
+    public class CalViewModel : INotifyPropertyChanged
+    {
+        private CalModel myModel = null;
+
+        public CalViewModel()
+        {
+            myModel = new CalModel();
+            
+        }
+        public string Dollar
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(myModel.dollar))
+                {
+                    Won = "0";
+                }
+                else
+                {
+                    int num = -1;
+                    if (int.TryParse(myModel.dollar, out num))
+                    {
+                        int result = num * 1160;
+                        Won = result.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please insert number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        Dollar = "";
+                        Won = "0";
+                    }
+                }
+                return myModel.dollar;
+            }
+            set
+            {
+                if (myModel.dollar != value)
+                {
+                    myModel.dollar = value;
+                    OnPropertyChanged("Dollar");
+                }
+            }
+        }
+
+        public string Won
+        {
+            get
+            {
+                return myModel.won;
+            }
+            set
+            {
+                myModel.won = value;
+                OnPropertyChanged("Won");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
